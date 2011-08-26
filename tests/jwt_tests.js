f21b685792621122d2ab0cc65fdc2a211c991418
Richard Anaya <richard.anaya@gmail.com>
2011-08-26 13:49:56 -0500

    adding first test

---
+++ tests/jwt_tests.js
@@ -0,0 +1,16 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    jwt = require('../jwt');
+
+vows.describe('JWT').addBatch({
+    'A simple json object': {
+        topic: {"foo":"bar"},
+
+        'encodes and decodes': function (payload) {
+            var secret = "secret"
+            var encoded_payload = jwt.encode(payload, secret);
+            var decoded_payload = jwt.decode(encoded_payload, secret);
+            assert.equal(payload, decoded_payload);
+        }
+    }
+}).export(module); // Export the Suite
